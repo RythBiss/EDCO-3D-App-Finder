a45febdcf41893d5b24f618972a20c7a285f0a6d
RythBiss <dkschaarschmidt@gmail.com>
2024-05-28 16:39:12 -0400

    Updated List Button Style
    Updated list button style to look less blocky.

--- edco_sim/src/components/ListButton.tsx
+++ edco_sim/src/components/ListButton.tsx
@@ -28,18 +28,19 @@ export default function ListButton(props: any) {
         <div className='row bottom-gap'>
             <div className={`col-${props.indent}`}/>
             <div className='col tab-bar'>
-                <button type="button" className={`btn-wrapper- list-btn w-100 container ${props.active == true ? 'btn-active' : ''}`} onClick={handleOnClick}>
+                <button type="button" className={`btn-wrapper ${props.icon ? 'list-btn-icon' : 'list-btn'} w-100 container`} onClick={handleOnClick}>
                     <div className={`row ${props.icon ? 'justify-content-around' : ''}`}>
                         {props.icon ?
                             <>
+                                <div className='icon-cushion'/>
                                 <img
                                     src={props.icon}
                                     alt='machine-icon'
                                     width={128}
                                     className='machine-icon-frame col-4'
                                 />
-                                <div className="col" ></div>
-                                <div className="col-4 vert-space-between">
+                                <div className={`col btn-icon-spaces ${props.active == true ? 'btn-active' : 'vert-space-between'}`} ></div>
+                                <div className={`col-4 btn-icon-spaces ${props.active == true ? 'btn-active' : 'vert-space-between'}`}>
                                     <div>
                                         <div className='col-12 text-end list-btn-inner'>{props.lable}</div>
                                     </div>
